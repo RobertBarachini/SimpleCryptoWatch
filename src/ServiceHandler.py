2b97229240a2a63880fa2b404d294d4023a53ffd
robert_barachini <robert.barachini@gmail.com>
2021-05-13 00:24:27 +0200

    Implement basic support for Windows Services.

---
+++ src/ServiceHandler.py
@@ -0,0 +1,46 @@
+import win32serviceutil
+import win32service
+import win32event
+import servicemanager
+import socket
+import os
+import config
+import SupportFunctions as sf
+import DataGrabber as dg
+
+log = sf.set_logging("ServiceHandler")
+machine = os.environ['COMPUTERNAME']
+
+def is_installed(name):
+	return service_status(name) != None
+		
+def is_running(name):
+	rez = service_status(name)
+	if rez and rez[1] == 4:
+		return True
+	else:
+		return False
+	# return rez and rez[1] == 4 # Returns None if rez is None
+
+def service_status(name):
+	try:
+		log.info(f"Checking status for service '{name}'")
+		rez = win32serviceutil.QueryServiceStatus(name, machine)
+		log.info(f"Service '{name}' running: '{rez}'")
+		return rez
+	except Exception as e:
+		log.error(e)
+		return None
+		# if "The specified service does not exist as an installed service" in str(e):
+		# 	None
+
+def start_service(name):
+	 win32serviceutil.StartService(name, machine)
+
+if __name__ == "__main__":
+	name = "Windows Search"
+	# name = "hmmmmmmmmmmmmmm"
+	running = is_running(name)
+	installed = is_installed(name)
+	print(machine)
+	print(f"Service {name} is running: {running} ; is installed: {installed}")
--- src/config.py
+++ src/config.py
@@ -4,3 +4,4 @@ environment = "debug" # debug / production
 interval_coinmarketcap = 5 * 60 # Refresh interval for coinmarketcap in seconds
 tablename_livelistings = "live_listings"
 coinmarketcap_limit = 100
+service_crypto = "SimpleCryptoWatch"
